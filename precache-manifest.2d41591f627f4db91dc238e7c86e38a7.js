1df3286e0e973cefc3aa9b3d98441fccc088bf68
Anton Balashov <balashov.anton.1983@gmail.com>
2020-08-05 14:51:38 +0300

    Updates

---
+++ precache-manifest.2d41591f627f4db91dc238e7c86e38a7.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3206cb6ef9c6b887d2b9523ec24e505a",
+    "url": "/test-front/index.html"
+  },
+  {
+    "revision": "c46bed289b0c15e19309",
+    "url": "/test-front/static/css/main.4aa6f008.chunk.css"
+  },
+  {
+    "revision": "6950e0ad2587e0071740",
+    "url": "/test-front/static/js/2.86ed5d30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-front/static/js/2.86ed5d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c46bed289b0c15e19309",
+    "url": "/test-front/static/js/main.3e8c1201.chunk.js"
+  },
+  {
+    "revision": "c88ec16f208308a1e337",
+    "url": "/test-front/static/js/runtime-main.e09485ec.js"
+  },
+  {
+    "revision": "30cd20a3d8e547f317554f9848a74dea",
+    "url": "/test-front/static/media/KrinkesDecorPERSONALUSE.30cd20a3.woff"
+  },
+  {
+    "revision": "ab14dbba98f5da4e18064ddf3dfe7d77",
+    "url": "/test-front/static/media/KrinkesDecorPERSONALUSE.ab14dbba.woff2"
+  },
+  {
+    "revision": "b3b67a446e40c93b844dad04ce120bda",
+    "url": "/test-front/static/media/KrinkesDecorPERSONALUSE.b3b67a44.eot"
+  },
+  {
+    "revision": "f8ae97e3486a171946ddc6168e9db111",
+    "url": "/test-front/static/media/KrinkesDecorPERSONALUSE.f8ae97e3.ttf"
+  },
+  {
+    "revision": "0f37fb6af5c7332d60586416b2ffde37",
+    "url": "/test-front/static/media/Lato-Bold.0f37fb6a.eot"
+  },
+  {
+    "revision": "12a234e87f37d441adc9bb2e3dec13de",
+    "url": "/test-front/static/media/Lato-Bold.12a234e8.woff2"
+  },
+  {
+    "revision": "2032f2b161a7a109a9d0cc3f6ad3b735",
+    "url": "/test-front/static/media/Lato-Bold.2032f2b1.woff"
+  },
+  {
+    "revision": "ff2b668b4f34a2c1c110e92ae1c01f56",
+    "url": "/test-front/static/media/Lato-Bold.ff2b668b.ttf"
+  },
+  {
+    "revision": "69279aef7fbc11101022a9f06079bbbf",
+    "url": "/test-front/static/media/Lato-Regular.69279aef.woff2"
+  },
+  {
+    "revision": "ba0cc8b032cbbab84a9c0990bfcb2087",
+    "url": "/test-front/static/media/Lato-Regular.ba0cc8b0.ttf"
+  },
+  {
+    "revision": "e87e43c57f21dd637791c6bfa534c1af",
+    "url": "/test-front/static/media/Lato-Regular.e87e43c5.woff"
+  },
+  {
+    "revision": "fb529b5c4d5892b07fa311b71553c4ed",
+    "url": "/test-front/static/media/Lato-Regular.fb529b5c.eot"
+  },
+  {
+    "revision": "0e799f6e29b47484898cb785f1ec3e2d",
+    "url": "/test-front/static/media/PlayfairDisplay-BoldItalic.0e799f6e.woff2"
+  },
+  {
+    "revision": "47f06cd3c4eea5d0f8fd0747b05969bf",
+    "url": "/test-front/static/media/PlayfairDisplay-BoldItalic.47f06cd3.eot"
+  },
+  {
+    "revision": "487bf8361dfbe9b8ed5297de0ef5a228",
+    "url": "/test-front/static/media/PlayfairDisplay-BoldItalic.487bf836.ttf"
+  },
+  {
+    "revision": "eea1a018e36a9886f64a5dbbe7a07bc4",
+    "url": "/test-front/static/media/PlayfairDisplay-BoldItalic.eea1a018.woff"
+  },
+  {
+    "revision": "05095e4917d34ecf8365146922dd9a48",
+    "url": "/test-front/static/media/PlayfairDisplay-Italic.05095e49.eot"
+  },
+  {
+    "revision": "1afd21e76064d0edd7b351fd1cb8c619",
+    "url": "/test-front/static/media/PlayfairDisplay-Italic.1afd21e7.woff2"
+  },
+  {
+    "revision": "75d4e58645673553df9b0a97da9c2ff9",
+    "url": "/test-front/static/media/PlayfairDisplay-Italic.75d4e586.ttf"
+  },
+  {
+    "revision": "cbb6b4179fc691e9f57182054e059fc1",
+    "url": "/test-front/static/media/PlayfairDisplay-Italic.cbb6b417.woff"
+  },
+  {
+    "revision": "874c9ef7872ae1ebe9600fb79cf6bf15",
+    "url": "/test-front/static/media/header-slide-1.874c9ef7.png"
+  },
+  {
+    "revision": "082320d3c6442dff8b8ef94f35967275",
+    "url": "/test-front/static/media/icon-clock.082320d3.svg"
+  },
+  {
+    "revision": "1be60f4eac19e5f60b3e78ba2f469f12",
+    "url": "/test-front/static/media/icon-plane.1be60f4e.svg"
+  },
+  {
+    "revision": "d20b2efdfb35a04c73c4eac28247e16a",
+    "url": "/test-front/static/media/icon-question.d20b2efd.svg"
+  }
+]);
