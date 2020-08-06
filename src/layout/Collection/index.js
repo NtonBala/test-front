ffe3ad488ec7c9d073ef980c6c6df8166cf9627c
Anton Balashov <balashov.anton.1983@gmail.com>
2020-08-06 17:45:50 +0300

    Add Collection grid

--- src/layout/Collection/index.js
+++ src/layout/Collection/index.js
@@ -7,16 +7,64 @@ import { SectionTitle, CollectionCard } from '../../components';
 // Instruments
 import './Collection.scss';
 import collectionImg1 from '../../images/collection-1.png';
+import collectionImg2 from '../../images/collection-2.png';
+import collectionImg3 from '../../images/collection-3.png';
+import collectionImg4 from '../../images/collection-4.png';
+import collectionImg5 from '../../images/collection-5.png';
+import collectionImg6 from '../../images/collection-6.png';
+import collectionImg7 from '../../images/collection-7.png';
+import collectionImg8 from '../../images/collection-8.png';
+
+const cards = [
+  {
+    imgSrc: collectionImg1,
+    discount: 20,
+    buttonAccent: true,
+  },
+  {
+    imgSrc: collectionImg2,
+  },
+  {
+    imgSrc: collectionImg3,
+  },
+  {
+    imgSrc: collectionImg4,
+    discount: 40,
+  },
+  {
+    imgSrc: collectionImg5,
+    discount: 20,
+    buttonAccent: true,
+  },
+  {
+    imgSrc: collectionImg6,
+  },
+  {
+    imgSrc: collectionImg7,
+  },
+  {
+    imgSrc: collectionImg8,
+    discount: 40,
+  },
+];
 
 const Collection = () => {
+  const cardsJSX = cards.map((item) => {
+    return (
+      <div className="collection__card-wrapper">
+        <CollectionCard { ...item } />
+      </div>
+    );
+  });
+
   return (
     <div className="collection">
       <div className="collection__title-wrapper">
         <SectionTitle>Summer Collection</SectionTitle>
       </div>
 
-      <div>
-        <CollectionCard imgSrc={ collectionImg1 } discount={ 20 } />
+      <div className="collection__grid-wrapper">
+        { cardsJSX }
       </div>
     </div>
   );
